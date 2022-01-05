87e6adb8742a77d4ed3623a1ad829b2d0c349e8e
Goulin Khoge <goulin.khoge@gmail.com>
2022-01-05 13:49:23 +0100

    add projects list

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,7 +16,11 @@ const About: NextPage = () => {
         <div className="opacity-75 flex items-center justify-center">
           <LocationMarkerIcon className="h-5 w-5 mr-2" /> France, Toulouse
         </div>
-        <p className="text-left prose prose-zinc dark:prose-invert max-w-full">
+        <p
+          className="text-left prose prose-zinc dark:prose-invert max-w-full
+          prose-a:no-underline hover:prose-a:underline
+          "
+        >
           I am a software engineer, I mostly do fullstack, DevOps and automation
           stuff 🤖. Coding has been my passion for{" "}
           {new Date().getFullYear() - 2016} years and I am always eager to learn
@@ -32,6 +36,41 @@ const About: NextPage = () => {
           alternatives as side projects to try to replace platforms that we use
           daily and that are very closed software and not privacy friendly.
         </p>
+      </section>
+      <section
+        className="max-w-container mx-auto my-16 prose prose-zinc dark:prose-invert
+        prose-a:no-underline hover:prose-a:underline
+      "
+      >
+        <h2>Side projects</h2>
+        <ul>
+          <li>
+            <a href="https://github.com/goulinkh/podcast-cli">
+              <code>Podcast-cli</code>
+            </a>
+            : Top-like interface for listening to podcasts podcast-cli lets you
+            play your favorite podcasts from the terminal
+          </li>
+          <li>
+            <a href="https://github.com/goulinkh/rooms-finder">
+              Free rooms finder
+            </a>
+            : A Web app that helps the students of the University Paul Sabatier
+            to easily find free amphitheaters and class rooms
+          </li>
+          <li>
+            <a href="https://github.com/goulinkh/vanilla-framework-intellisense">
+              Vanilla Framework Intellisense
+            </a>
+            : A VS Code extension that provides intelligent suggestions for the
+            CSS library: &quot;
+            <a href="https://vanillaframework.io/">Vanilla framework</a>&quot;
+          </li>
+          <li>
+            <a href="http://ecommerce.goulin.fr/">E-commerce demo website</a>: A
+            fullstack e-commerce app, built using Nextjs, Prisma & Strapi CMS
+          </li>
+        </ul>
       </section>
     </>
   );
