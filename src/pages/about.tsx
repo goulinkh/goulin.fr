ef0aaabd85ef27002a9be5b872643471c11388af
Goulin Khoge <goulin.khoge@gmail.com>
2022-01-08 18:39:38 +0100

    update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import { LocationMarkerIcon } from "@heroicons/react/outline";
 import type { NextPage } from "next";
+import Link from "../components/common/Link";
 import Header from "../components/Header";
 import SEO from "../components/SEO";
 
@@ -14,25 +15,12 @@ const About: NextPage = () => {
         <div className="opacity-75 flex items-center justify-center">
           <LocationMarkerIcon className="h-5 w-5 mr-2" /> France, Toulouse
         </div>
-        <p
-          className="text-left prose prose-zinc dark:prose-invert max-w-full
-          prose-a:no-underline hover:prose-a:underline
-          "
-        >
+        <p className="text-left prose prose-zinc dark:prose-invert max-w-full">
           I am a software engineer, I mostly do fullstack, DevOps and automation
           stuff 🤖. Coding has been my passion for{" "}
           {new Date().getFullYear() - 2016} years and I am always eager to learn
           more about new technologies and to improve myself.
           <br />
-          Recently I&lsquo;ve been spending a lot of my free time on developing{" "}
-          <a
-            href="https://en.wikipedia.org/wiki/Free_and_open-source_software"
-            title="FOSS wiki page"
-          >
-            FOSS
-          </a>{" "}
-          alternatives as side projects to try to replace platforms that we use
-          daily and that are very closed software and not privacy friendly.
         </p>
       </section>
       <section
@@ -43,30 +31,35 @@ const About: NextPage = () => {
         <h2>Side projects</h2>
         <ul>
           <li>
-            <a href="https://github.com/goulinkh/podcast-cli">
+            <Link href="https://github.com/goulinkh/podcast-cli">
               <code>Podcast-cli</code>
-            </a>
+            </Link>
             : Top-like interface for listening to podcasts podcast-cli lets you
             play your favorite podcasts from the terminal
           </li>
           <li>
-            <a href="https://github.com/goulinkh/rooms-finder">
-              Free rooms finder
-            </a>
+            <Link href="https://github.com/goulinkh/rooms-finder">
+              <code>Free rooms finder</code>
+            </Link>
             : A Web app that helps the students of the University Paul Sabatier
             to easily find free amphitheaters and class rooms
           </li>
           <li>
-            <a href="https://github.com/goulinkh/vanilla-framework-intellisense">
-              Vanilla Framework Intellisense
-            </a>
+            <Link href="https://github.com/goulinkh/vanilla-framework-intellisense">
+              <code>Vanilla Framework Intellisense</code>
+            </Link>
             : A VS Code extension that provides intelligent suggestions for the
-            CSS library: &quot;
-            <a href="https://vanillaframework.io/">Vanilla framework</a>&quot;
+            CSS library:
+            <Link href="https://vanillaframework.io/">
+              &quot;Vanilla framework&quot;
+            </Link>
           </li>
           <li>
-            <a href="http://ecommerce.goulin.fr/">E-commerce demo website</a>: A
-            fullstack e-commerce app, built using Nextjs, Prisma & Strapi CMS
+            <Link href="http://ecommerce.goulin.fr/">
+              <code>E-commerce demo website</code>
+            </Link>
+            : A fullstack e-commerce app, built using Nextjs, Prisma & Strapi
+            CMS
           </li>
         </ul>
       </section>
